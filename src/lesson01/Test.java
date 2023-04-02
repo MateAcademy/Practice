4bdd00f09dc562598d24f406bd2da6f52130ec27
Sergey Klunniy <s.klunniy@gmail.com>
2023-04-02 12:40:22 +0300

    added interesting subject

---
+++ src/lesson01/Test.java
@@ -0,0 +1,15 @@
+package lesson01;
+
+/**
+ * @author Serhii Klunniy
+ */
+public class Test {
+    public static void main(String[] args) {
+        Integer a = 200;
+        Integer b = 200;
+
+        int c = 2 + 2;
+
+        System.out./* This is comment */println(a == b);
+    }
+}
