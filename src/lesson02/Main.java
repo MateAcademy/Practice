0087bd19a5c7216f65464671b3e9adb20e0e302e
Sergey Klunniy <s.klunniy@gmail.com>
2023-04-09 20:38:13 +0300

    added pull string

---
+++ src/lesson02/Main.java
@@ -0,0 +1,16 @@
+package lesson02;
+
+import java.util.Date;
+
+/**
+ * @author Serhii Klunniy
+ */
+public class Main {
+    public static void main(String[] args) {
+        Date date = new Date(5);
+        Date now = new Date(4);
+
+        System.out.println(date.after(now));
+Iterable
+    }
+}
