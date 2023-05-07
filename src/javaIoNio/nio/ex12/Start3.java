3ee3ac66a8de4376618fb9a30041ca9b3856a180
Sergey Klunniy <s.klunniy@gmail.com>
2023-05-07 21:02:08 +0300

    add edits

--- src/javaIoNio/nio/ex12/Start3.java
+++ src/javaIoNio/nio/ex12/Start3.java
@@ -1,9 +1,7 @@
 package javaIoNio.nio.ex12;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 /**
  * @author Serhii Klunniy
@@ -21,6 +19,29 @@ public class Start3 {
             }
 
             Files.createDirectories(Paths.get("temp/a/b/c"));
+
+            Files.createTempDirectory(dir, "tmp_");
+
+//            Iterable<Path> rootDirectories = FileSystems.getDefault().getRootDirectories();
+//            for (Path rootDir : rootDirectories) {
+//                System.out.println(rootDir);
+//            }
+
+            DirectoryStream.Filter<Path> filter = new DirectoryStream.Filter<Path>() {
+                @Override
+                public boolean accept(Path entry) throws IOException {
+                    return Files.isDirectory(entry);
+                }
+            };
+
+           try(DirectoryStream<Path> paths = Files.newDirectoryStream(dir, "*.{txt, bin}")) {
+               for (Path rootDir : paths) {
+                   System.out.println(rootDir);
+               }
+           }
+
+            System.out.println(Files.isDirectory(Paths.get("D:\\projectsJAVA\\Practice\\temp\\temp")));
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
