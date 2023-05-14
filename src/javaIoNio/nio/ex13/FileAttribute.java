9bde9e6b6da94289844d3fc7e0bbf13c79498c37
Sergey Klunniy <s.klunniy@gmail.com>
2023-05-14 18:33:51 +0300

    add edits

---
+++ src/javaIoNio/nio/ex13/FileAttribute.java
@@ -0,0 +1,57 @@
+package javaIoNio.nio.ex13;
+
+import java.io.IOException;
+import java.nio.file.FileStore;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.*;
+import java.util.Set;
+
+/**
+ * @author Serhii Klunniy
+ */
+public class FileAttribute {
+    public static void main(String[] args) throws IOException {
+        Path path = Path.of("D:\\projectsJAVA\\Practice\\src\\javaIoNio\\nio\\ex13\\test.txt");
+
+        System.out.println("Size " + Files.size(path));
+        System.out.println("Date modified " + Files.getLastModifiedTime(path));
+        System.out.println("Is writable " + Files.isWritable(path));
+
+        //FileStore - это репрезентация нашего файлового хранилища, например логического диска
+        FileStore fileStore = Files.getFileStore(path);
+//        System.out.println(fileStore.getTotalSpace());
+//        System.out.println("Type " + fileStore.type());
+
+        validateView (fileStore, BasicFileAttributeView.class);
+        validateView (fileStore, DosFileAttributeView.class);
+        validateView (fileStore, PosixFileAttributeView.class);
+        validateView (fileStore, AclFileAttributeView.class);
+        validateView (fileStore, UserDefinedFileAttributeView.class);
+        validateView (fileStore, FileOwnerAttributeView.class);
+
+//        1. BasicFileAttributeView - этот набор атрибутов поддерживается всеми операционными системами
+//        2. DosFileAttributeView - windows
+//        3. PosixFileAttributeView - no windows, read hart, centos, ubunty
+//        4. AclFileAttributeView - позволяет управлять правами доступа к файлам и папкам на платформе windows
+//        5. UserDefinedFileAttributeView
+//        6. FileOwnerAttributeView
+
+//        DosFileAttributes dosFileAttributes = Files.readAttributes(path, DosFileAttributes.class);
+//        System.out.println("Creation time " + dosFileAttributes.creationTime());
+//        System.out.println("Is hidden " + dosFileAttributes.isHidden());
+
+        Set<PosixFilePermission> posixFilePermissions = PosixFilePermissions.fromString("rwx------");
+        java.nio.file.attribute.FileAttribute<Set<PosixFilePermission>> setFileAttribute = PosixFilePermissions.asFileAttribute(posixFilePermissions);
+        Files.createFile(Paths.get("file1.txt"), setFileAttribute);
+
+
+    }
+
+    private static void validateView(FileStore fileStore, Class<? extends FileAttributeView> viewClass) {
+        boolean supports = fileStore.supportsFileAttributeView(viewClass);
+        System.out.println("supports " + viewClass.getSimpleName() + " " +  supports);
+    }
+
+}
