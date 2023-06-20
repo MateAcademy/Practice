b68d30d9611cea1edd4cedcebfbf6315fbd2f410
Serhii Klunniy <s.klunniy@gmail.com>
2023-06-20 17:34:21 +0300

    add socket scanner

--- src/hillel_shaytan/lesson23/socket/Client.java
+++ src/hillel_shaytan/lesson23/socket/Client.java
@@ -7,11 +7,16 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
         try (Socket socket = new Socket("localhost", 8085)) {
 
             PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
 
-            printWriter.println("hello my socket");
+            while (true) {
+                printWriter.println("hello my socket");
+                printWriter.println(scanner.nextLine());
+            }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
