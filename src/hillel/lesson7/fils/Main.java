8a1f46b7c6737e9bddd3532c0691c6da807ced8e
Serhii Klunniy <s.klunniy@gmail.com>
2023-06-20 12:51:54 +0300

    add files

--- src/hillel/lesson7/fils/Main.java
+++ src/hillel/lesson7/fils/Main.java
@@ -2,6 +2,10 @@ package hillel.lesson7.fils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Date;
 
 public class Main {
@@ -24,5 +28,29 @@ public class Main {
 
        long data =  testFile.lastModified(); //отчет времени в программировании 1 янв 1970, мы должны увидеть количество секунд пройденных с этого времени
         System.out.println(new Date(data));
+
+        //java nio  - лучше работает с точки зрения многопоточности
+        //          - немного быстрее
+        //          - нейминг более логичный
+
+        //nio
+        Path path = Paths.get("test.txt");
+        //Files.createFile(path);
+        // мы можем конвертировать file в path и наоборот
+        File fromPath = path.toFile();
+        Path fromFile = fromPath.toPath();
+
+        Path java = Paths.get("test1.txt");
+        Files.createFile(java);
+        Files.exists(java);
+        Files.size(java);
+        Files.getLastModifiedTime(java).toMillis(); //1231914805000
+        Files.copy(java, Paths.get("text.txt"), StandardCopyOption.REPLACE_EXISTING);
+
+        Path input = Paths.get("input.txt");
+        Path path2 = Paths.get("test.txt");
+
+        Files.copy(input, path2, StandardCopyOption.REPLACE_EXISTING);
+
     }
 }
