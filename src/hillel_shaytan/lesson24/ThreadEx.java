8f6cfa7215f47c662c653a5f01ef3a153dc301d0
Sergey Klunniy <s.klunniy@gmail.com>
2023-06-24 19:58:29 +0300

    add commit thread

--- src/hillel_shaytan/lesson24/ThreadEx.java
+++ src/hillel_shaytan/lesson24/ThreadEx.java
@@ -4,13 +4,19 @@ package hillel_shaytan.lesson24;
  * @author Serhii Klunniy
  */
 public class ThreadEx {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Runnable runnable = () -> System.out.println("hello from 2 thread");
 
         Thread second = new Thread(runnable);
         second.start();
 
-        Thread.sleep(2000);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("__main__");
 
     }
 }
