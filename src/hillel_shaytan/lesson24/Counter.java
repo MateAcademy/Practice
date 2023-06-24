daa272455b0befb0b960aaa7570d1c45d50b830a
Sergey Klunniy <s.klunniy@gmail.com>
2023-06-24 21:26:48 +0300

    thread

---
+++ src/hillel_shaytan/lesson24/Counter.java
@@ -0,0 +1,39 @@
+package hillel_shaytan.lesson24;
+
+import java.util.Random;
+
+/**
+ * @author Serhii Klunniy
+ */
+public class Counter {
+
+    public int days;
+    public int half;
+
+    public void mix() {
+        half++;
+        try {
+            Thread.sleep(new Random().nextInt(20));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        days += 1;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+        Runnable runnable = () -> {
+            for (int i = 0; i < 100; i++) counter.mix();
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
+
+        for (int i = 0; i < 100; i++) counter.mix();
+
+        thread.join();
+
+        System.out.println(counter.half); //198
+        System.out.println(counter.days); //199
+    }
+}
